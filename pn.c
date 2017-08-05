60a1605ec6958f4b88cc56420a988633620d4cc7
Dhivithana <30549440+Dhivithana@users.noreply.github.com>
2017-08-05 16:34:51 +0530

    Create pn.c

---
+++ pn.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int a;
+printf("enter a value:");
+scanf("%d",&a);
+if(a>0)
+{
+printf("given number is positive");
+}
+else
+{
+printf("given number is not positive);
+}
+getch();
+}
