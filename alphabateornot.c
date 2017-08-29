fcea379961253aee04422d3f203716a471326b0f
Dhivithana <30549440+Dhivithana@users.noreply.github.com>
2017-08-29 15:40:39 +0530

    Create alphabateornot.c

---
+++ alphabateornot.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int a;
+printf("enter the value:");
+scanf("%d",&c);
+if((a>=a&&a<=z)||(a<=A&&a>=Z))
+{
+printf("alphabate");
+}
+else
+{
+printf("number");
+}
+getch();
+}
