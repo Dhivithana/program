b1bb9baa7bb34a91e5b799d704f47a5f50ac3c5f
Dhivithana <30549440+Dhivithana@users.noreply.github.com>
2017-08-29 15:09:57 +0530

    Create vowelornot.c

---
+++ vowelornot.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+char c,upper,lower;
+clrscr();
+printf("enter the character");
+scanf("%c",&c);
+if(c=='a'||c=='A'||c=='e'||c=='E'||c=='i'||c=='I'||c=='o'||c=='O'||c=='u'||c=='U')
+{
+printf("the given character is vowel");
+}
+else
+{
+printf("the given character is not a vowel");
+}
+return 0;
+}
