4d5bc74fd26c70444f4d056ba6ea7154dbb5be96
Dhivithana <30549440+Dhivithana@users.noreply.github.com>
2018-01-10 16:52:20 +0530

    Create palindrome.c

---
+++ palindrome.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+	int num;
+	printf("enter the number ");
+	scanf("%d",&num);
+	if(num==num)
+	{
+	printf("yes");
+	}
+	else
+	{
+		printf("no");
+		
+	}
+	return 0;
+}
