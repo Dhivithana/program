215724f008437e9f4edefee8c1668d1109ff1821
Dhivithana <30549440+Dhivithana@users.noreply.github.com>
2018-01-31 16:19:31 +0530

    Create numerictest.c

---
+++ numerictest.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+int n;
+printf("\n Enter value to be checked for numeric:");
+scanf("%d",&n);
+if(n<=9)
+{
+printf("\n Yes");
+}
+else
+{
+printf("\n No");
+}
+return 0;
+}
