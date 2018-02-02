393acff202a263240d3515553eb280d7fbc03b71
Dhivithana <30549440+Dhivithana@users.noreply.github.com>
2018-02-02 16:10:06 +0530

    Create maxarray.c

---
+++ maxarray.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+int main()
+{
+int a[50];
+int size,elements;
+int f=0;
+printf("\n enter the size of your array:");
+scanf("%d",&size);
+printf("\n enter the elements of your array:");
+for(i=0;i<=n;i++)
+{
+scanf("%d",&a[i]);
+}
+for(i=0;i<=n;i++)
+{
+if(a[i]>f)
+{
+f=a[i];
+printf("\n The maximum element is:%d",f);
+}
+}
+return 0;
+}
