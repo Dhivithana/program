f790ec94a23480736161ae55aadd0c04ae4bd16b
Dhivithana <30549440+Dhivithana@users.noreply.github.com>
2018-02-05 16:38:25 +0530

    Create swapnum.c

---
+++ swapnum.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+int a,temp,b;
+printf("\n enter two numbers:");
+scanf("%d%d",&a,&b);
+printf("\n Numbers before swap(%d,%d):",a,b);
+temp=a;
+a=b;
+b=temp;
+printf("\n The swapped result is:(%d,%d)",a,b);
+return 0;
+}
