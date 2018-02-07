7061757309a928974b1e8aa8a1710c9a79540b43
Dhivithana <30549440+Dhivithana@users.noreply.github.com>
2018-02-07 16:27:12 +0530

    Update maxarray.c

--- maxarray.c
+++ maxarray.c
@@ -2,22 +2,22 @@
 int main()
 {
 int a[50];
-int size,elements;
+int size,i;
 int f=0;
 printf("\n enter the size of your array:");
 scanf("%d",&size);
 printf("\n enter the elements of your array:");
-for(i=0;i<=n;i++)
+for(i=0;i<=size;i++)
 {
 scanf("%d",&a[i]);
 }
-for(i=0;i<=n;i++)
+for(i=0;i<=size;i++)
 {
 if(a[i]>f)
 {
 f=a[i];
+}
+}
 printf("\n The maximum element is:%d",f);
-}
-}
 return 0;
 }
