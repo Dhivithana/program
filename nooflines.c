9b0c2ff096c6643d881b2671d6e74424c28c50f5
Dhivithana <30549440+Dhivithana@users.noreply.github.com>
2018-02-07 16:51:15 +0530

    Create nooflines.c

---
+++ nooflines.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+char str[50];
+int c=0,i;
+printf("\n Enter your string:");
+gets(str);
+for(i=0;str[i]!=0;i++)
+{
+if(str[i]=='.')
+{
+c++;
+}
+}
+printf("\n The number of spaces are%d:",c+1);
+return 0;
+}
