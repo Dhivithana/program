8f50f5ad83e09c67c20ba15a9f62564c6d805878
Dhivithana <30549440+Dhivithana@users.noreply.github.com>
2018-02-07 16:56:59 +0530

    Update spaces.c

--- spaces.c
+++ spaces.c
@@ -2,18 +2,18 @@
 #include<string.h>
 int main()
 {
-	char str[50];
-	int c=1,i;
-	printf("\n Enter your string:");
-	gets(str);
-	for(i=0;str[i]!='\0';i++)
-	{
-		if(str[i]==' ')
-		{
-
-			c++;
-	}
-	}
-	printf("\n The number of spaces are%d:",c);
-	return 0;
+char str[50];
+int c=0,i;
+printf("\n Enter your string:");
+gets(str);
+for(i=0;str[i]!=0;i++)
+{
+if(str[i]==' ')
+{
+c++;
 }
+}
+printf("\n The number of spaces are%d:",c);
+return 0;
+}
+
