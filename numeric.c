ed53ad5cf54976a8117df91461d7657c85c65a46
Dhivithana <30549440+Dhivithana@users.noreply.github.com>
2018-02-09 15:57:19 +0530

    Create numeric.c

---
+++ numeric.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include<string.h>
+int main(void) {
+char a[10];
+gets(a);
+int i,c=0;
+printf("\n enter your string:");
+for(i=0;a[i]!='\0';i++)
+{
+if(a[i]>='0' && a[i]<='9')
+{
+c++;
+}
+}
+printf("\n The numeric numbers are:%d",c);
+return 0;
+}
