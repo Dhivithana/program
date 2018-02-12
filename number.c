22f9e4c4cfa62e34b75141b4b037a3eaf7ca5fa5
Dhivithana <30549440+Dhivithana@users.noreply.github.com>
2018-02-12 15:56:57 +0530

    Create number.c

---
+++ number.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+void main()
+{
+	int n;
+	printf("\n Enter your number:");
+	scanf("%d",&n);
+	if(n>=1 && n<=10)
+	{
+		printf("\n Yes");
+		
+	}
+	else
+	{
+		printf("\n NO");
+		
+	}
+}
